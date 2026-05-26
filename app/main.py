5c922f8f02014504e65ac3391834f5547aa03a4a
Sardor <sardor.tozhiev1994@gmail.com>
2026-05-26 13:44:18 +0300

    work is done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if parts[0] != "mv":
+        return
+
+    source = parts[1]
+    destination = parts[2]
+
+    if destination.endswith("/"):
+        destination = destination + os.path.basename(source)
+
+    dest_dir = os.path.dirname(destination)
+    if dest_dir:
+        parts_dir = dest_dir.split("/")
+        current_path = ""
+        for part in parts_dir:
+            if not current_path:
+                current_path = part
+            else:
+                current_path = current_path + "/" + part
+            if not os.path.exists(current_path):
+                os.mkdir(current_path)
+
+    os.rename(source, destination)
