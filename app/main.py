fdf5d2298bd49172d99a23921ab4d589baa19140
Sardor <sardor.tozhiev1994@gmail.com>
2026-05-26 16:21:48 +0300

    fix: final cleanup

--- app/main.py
+++ app/main.py
@@ -14,14 +14,6 @@ def move_file(command: str) -> None:
 
     dest_dir = os.path.dirname(destination)
     if dest_dir:
-        # parts_dir = dest_dir.split("/")
-        # current_path = ""
-        # for part in parts_dir:
-        #     if not current_path:
-        #         current_path = part
-        #     else:
-        #         current_path = os.path.join(current_path, part)
-        #     if not os.path.exists(current_path):
         os.makedirs(dest_dir, exist_ok=True)
 
     os.rename(source, destination)
